b4b27027cb4877ea729b6ccdb0eb0d91cb6d75eb
dsorthep <detvisiths@yahoo.com>
2019-12-17 00:30:21 -0600

    set up routes and port and dependencies

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+
+
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
